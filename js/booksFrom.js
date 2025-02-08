f96d51e26d7173aecaecaafcd34c566a692b06ee
Nayra Ashraf <youssefnayra26@gmail.com>
2025-02-08 22:47:00 +0200

    make validation functions generic to use in books form and table

--- js/booksFrom.js
+++ js/booksFrom.js
@@ -1,73 +1,100 @@
 import { Book } from "./classes/bookClass.js";
 import { Author } from "./classes/authorClass.js";
 
-function bookNameValidate() {
-  let bookName = document.getElementById("bookName");
-  let span = document.getElementById("bookNameSpan");
+export function bookNameValidate(obj) {
+  let bookName = obj.querySelector("#bookName");
   if (!bookName.value.trim()) {
-    span.textContent = "Please enter a book name";
-    span.classList.remove("removeValidation");
-    span.classList.add("validation");
+    if (!obj.querySelector("#bookNameSpan")) {
+      obj;
+      let span = document.createElement("span");
+      span.textContent = "Please enter a book name";
+      span.id = "bookNameSpan";
+      span.classList.add("validation");
+      bookName.insertAdjacentElement("afterend", span);
+    }
   } else {
-    span.classList.remove("validation");
-    span.classList.add("removeValidation");
+    let span = obj.querySelector("#bookNameSpan");
+    if (span) {
+      span.remove();
+    }
     return bookName.value;
   }
 }
 
-function publishDateValidate() {
-  let publishDate = document.getElementById("publishDate");
-  let span = document.getElementById("publishDateSpan");
+export function publishDateValidate(obj) {
+  let publishDate = obj.querySelector("#publishDate");
   if (!publishDate.value) {
-    span.textContent = "Please enter a Publish Date";
-    span.classList.remove("removeValidation");
-    span.classList.add("validation");
+    if (!obj.querySelector("#publishDateSpan")) {
+      let span = document.createElement("span");
+      span.textContent = "Please enter a Publish Date";
+      span.id = "publishDateSpan";
+      span.classList.add("validation");
+      publishDate.insertAdjacentElement("afterend", span);
+    }
   } else {
-    span.classList.remove("validation");
-    span.classList.add("removeValidation");
+    let span = obj.querySelector("#publishDateSpan");
+    if (span) {
+      span.remove();
+    }
     return publishDate.value;
   }
 }
 
-function bookPriceValidate() {
-  let price = document.getElementById("price");
-  let span = document.getElementById("priceSpan");
+export function bookPriceValidate(obj) {
+  let price = obj.querySelector("#price");
   if (!price.value.trim() || isNaN(price.value)) {
-    span.textContent = "Please enter a book price";
-    span.classList.remove("removeValidation");
-    span.classList.add("validation");
+    if (!obj.querySelector("#priceSpan")) {
+      let span = document.createElement("span");
+      span.textContent = "Please enter a book price";
+      span.id = "priceSpan";
+      span.classList.add("validation");
+      price.insertAdjacentElement("afterend", span);
+    }
   } else {
-    span.classList.remove("validation");
-    span.classList.add("removeValidation");
+    let span = obj.querySelector("#priceSpan");
+    if (span) {
+      span.remove();
+    }
     return parseFloat(price.value);
   }
 }
 
-function authorNameValidate() {
-  let authorName = document.getElementById("authorName");
-  let span = document.getElementById("authorNameSpan");
+export function authorNameValidate(obj) {
+  let authorName = obj.querySelector("#authorName");
   if (!authorName.value.trim()) {
-    span.textContent = "Please enter an author name";
-    span.classList.remove("removeValidation");
-    span.classList.add("validation");
+    if (!obj.querySelector("#authorNameSpan")) {
+      let span = document.createElement("span");
+      span.textContent = "Please enter an author name";
+      span.id = "authorNameSpan";
+      span.classList.add("validation");
+      authorName.insertAdjacentElement("afterend", span);
+    }
   } else {
-    span.classList.remove("validation");
-    span.classList.add("removeValidation");
+    let span = obj.querySelector("#authorNameSpan");
+    if (span) {
+      span.remove();
+    }
     return authorName.value;
   }
 }
 
-function authorEmailValidate() {
-  let authorEmail = document.getElementById("authorEmail");
-  let regex = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$");
-  let span = document.getElementById("authorEmailSpan");
+export function authorEmailValidate(obj) {
+  let authorEmail = obj.querySelector("#authorEmail");
+  let regex = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+
   if (!regex.test(authorEmail.value)) {
-    span.textContent = "Please enter a valid email address";
-    span.classList.remove("removeValidation");
-    span.classList.add("validation");
+    if (!obj.querySelector("#authorEmailSpan")) {
+      let span = document.createElement("span");
+      span.textContent = "Please enter a valid email address";
+      span.id = "authorEmailSpan";
+      span.classList.add("validation");
+      authorEmail.insertAdjacentElement("afterend", span);
+    }
   } else {
-    span.classList.remove("validation");
-    span.classList.add("removeValidation");
+    let span = obj.querySelector("#authorEmailSpan");
+    if (span) {
+      span.remove();
+    }
     return authorEmail.value;
   }
 }
@@ -77,6 +104,7 @@ function reset() {
   document.getElementById("price").value = "";
   document.getElementById("authorName").value = "";
   document.getElementById("authorEmail").value = "";
+  document.getElementById("publishDate").value = "";
 }
 
 let booksArr = [];
@@ -84,11 +112,11 @@ let noBooks = parseInt(location.search.split("=")[1]) || 0;
 function addBook(e) {
   e.preventDefault();
 
-  let bookName = bookNameValidate();
-  let bookPrice = bookPriceValidate();
-  let authorName = authorNameValidate();
-  let authorEmail = authorEmailValidate();
-  let publishDate = publishDateValidate();
+  let bookName = bookNameValidate(document);
+  let bookPrice = bookPriceValidate(document);
+  let authorName = authorNameValidate(document);
+  let authorEmail = authorEmailValidate(document);
+  let publishDate = publishDateValidate(document);
   if (bookName && bookPrice && authorEmail && authorName && publishDate) {
     let author = new Author(authorName, authorEmail);
     let book = new Book(Date.now(), bookName, publishDate, bookPrice, author);
@@ -104,15 +132,28 @@ function addBook(e) {
   }
 }
 
-document
-  .getElementById("bookName")
-  .addEventListener("change", bookNameValidate);
-document.getElementById("price").addEventListener("change", bookPriceValidate);
-document
-  .getElementById("authorName")
-  .addEventListener("change", authorNameValidate);
-document
-  .getElementById("authorEmail")
-  .addEventListener("change", authorEmailValidate);
-document.querySelector("form").addEventListener("submit", addBook);
-document.getElementById("resetBtn").addEventListener("click", reset);
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("bookName").addEventListener("change", function () {
+    bookNameValidate(document);
+  });
+  document.getElementById("price").addEventListener("change", function () {
+    bookPriceValidate(document);
+  });
+  document
+    .getElementById("publishDate")
+    .addEventListener("change", function () {
+      publishDateValidate(document);
+    });
+  document.getElementById("authorName").addEventListener("change", function () {
+    authorNameValidate(document);
+  });
+  document
+    .getElementById("authorEmail")
+    .addEventListener("change", function () {
+      authorEmailValidate(document);
+    });
+  document.querySelector("form").addEventListener("submit", addBook);
+  document.getElementById("resetBtn").addEventListener("click", function () {
+    reset();
+  });
+});
--- js/booksTable.js
+++ js/booksTable.js
@@ -1,7 +1,16 @@
+import {
+  authorEmailValidate,
+  authorNameValidate,
+  bookNameValidate,
+  bookPriceValidate,
+  publishDateValidate,
+} from "./booksFrom.js";
+
 populateTable();
 
 function populateTable() {
   let books = JSON.parse(localStorage.getItem("booksArr")) || [];
+  console.log(books);
   const table = document.querySelector("table");
   table.innerHTML = `
   <thead>
@@ -20,19 +29,32 @@ function populateTable() {
     books.forEach((book) => {
       const tr = document.createElement("tr");
       tr.id = book.id;
-      tr.innerHTML = `<td data-label=name><span>${book.name}</span><input type="text" class="hide"/></td>
-        <td data-label=publishDate><span>${book.publishDate}</span><input type="date" class="hide"/></td>
-        <td data-label=price><span>$${book.price}</span><input type="number" class="hide"/></td>
-        <td data-label=authorName><span>${book.author.name}</span><input type="text" class="hide"/></td>
-        <td data-label=authorEmail><span>${book.author.email}</span><input type="email" class="hide"/></td>
+      tr.innerHTML = `<td data-label="name"><span>${book.name}</span><input type="text" class="hide" id="bookName"/></td>
+        <td data-label="publishDate"><span>${book.publishDate}</span><input type="date" class="hide" id="publishDate"/></td>
+        <td data-label="price"><span>$${book.price}</span><input type="number" class="hide" id="price"/></td>
+        <td data-label="authorName"><span>${book.author.name}</span><input type="text" class="hide" id="authorName"/></td>
+        <td data-label="authorEmail"><span>${book.author.email}</span><input type="email" class="hide" id="authorEmail" required /></td>
         <td class="button-container">
-          <button class="edit-button" onclick="edit(event, ${book.id})">Edit</button>
-          <button class="delete-button" onclick="Delete(event, ${book.id})">Delete</button>
-          <button class="save-button hide" onclick="save(event, ${book.id})">Save</button>
-          <button class="cancel-button hide " onclick="cancel(event, ${book.id})">Cancel</button>
+          <button class="edit-button">Edit</button>
+          <button class="delete-button">Delete</button>
+          <button class="save-button hide">Save</button>
+          <button class="cancel-button hide">Cancel</button>
         </td>`;
 
       table.children[1].appendChild(tr);
+
+      tr.querySelector(".edit-button").addEventListener("click", (e) =>
+        edit(e, book.id)
+      );
+      tr.querySelector(".delete-button").addEventListener("click", (e) =>
+        Delete(e, book.id)
+      );
+      tr.querySelector(".save-button").addEventListener("click", (e) =>
+        save(e, book.id)
+      );
+      tr.querySelector(".cancel-button").addEventListener("click", (e) =>
+        cancel(e, book.id)
+      );
     });
   }
 }
@@ -46,7 +68,11 @@ function toggleButtonVisibility(td) {
 
 function toggleInputVisibility(td) {
   td.querySelector("input").classList.toggle("hide");
-  td.querySelector("span").classList.toggle("hide");
+  let span = td.querySelectorAll("span");
+  span[0].classList.toggle("hide");
+  if (span[1]) {
+    span[1].remove();
+  }
 }
 
 function cancel(e, id) {
@@ -60,6 +86,7 @@ function cancel(e, id) {
     }
   });
 }
+
 function edit(e, id) {
   const tr = document.getElementById(id);
   const booksArr = JSON.parse(localStorage.getItem("booksArr")) || [];
@@ -90,29 +117,37 @@ function save(e, id) {
   const tr = document.getElementById(id);
   const booksArr = JSON.parse(localStorage.getItem("booksArr")) || [];
   const book = booksArr.find((book) => book.id === id);
-  if (book) {
+  let bookName = bookNameValidate(tr);
+  let bookPrice = bookPriceValidate(tr);
+  let authorName = authorNameValidate(tr);
+  let authorEmail = authorEmailValidate(tr);
+  let publishDate = publishDateValidate(tr);
+  if (bookName && bookPrice && authorEmail && authorName && publishDate) {
+    book.name = bookName;
+    book.publishDate = publishDate;
+    book.price = bookPrice;
+    book.author.name = authorName;
+    book.author.email = authorEmail;
     Array.from(tr.children).forEach((td) => {
       if (!td.classList.contains("button-container")) {
         toggleInputVisibility(td);
-        const span = td.querySelector("span");
-        const input = td.querySelector("input");
+        const span = td.querySelectorAll("span")[0];
         const key = td.dataset.label;
         if (key === "authorName") {
-          book.author.name = input.value;
           span.textContent = book.author.name;
         } else if (key === "authorEmail") {
-          book.author.email = input.value;
           span.textContent = book.author.email;
+        } else if (key === "price") {
+          span.textContent = "$" + book.price;
         } else {
-          book[key] = input.value;
-          span.textContent = key === "price" ? "$" + input.value : input.value;
+          span.textContent = book[key];
         }
       } else {
         toggleButtonVisibility(td);
       }
     });
+    localStorage.setItem("booksArr", JSON.stringify(booksArr));
   }
-  localStorage.setItem("booksArr", JSON.stringify(booksArr));
 }
 
 function Delete(e, id) {
